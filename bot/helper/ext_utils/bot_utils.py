2d2d1e5f986a49faef9121ee9d867a64e2c46656
amitsha1 <121236130+amitsha1@users.noreply.github.com>
2023-09-04 11:12:17 +0530

    Update bot_utils.py

--- bot/helper/ext_utils/bot_utils.py
+++ bot/helper/ext_utils/bot_utils.py
@@ -181,10 +181,12 @@ def get_readable_message():
 
         if config_dict['DELETE_LINKS']:
             msg += f"\n┠• ☋ Task » {download.extra_details['mode']}"
+       
         else:
-            msg += f"\n┠•☋ Task » <a href='{download.message.link}'>{download.extra_details['mode']}</a>" 
-          msg += f"\n┠•⌼ User » </code> {tag}"
-          msg += f"\n┖⌀ /{BotCommands.CancelMirror}_{download.gid()}\n\n"
+            msg += f"\n• <code>Task     </code>» <a href='{download.message.link}'>{download.extra_details['mode']}</a>"
+
+        msg += f"\n• <code>User     </code>» {tag}"
+        msg += f"\n⚠️ /{BotCommands.CancelMirror}_{download.gid()}\n\n"
 
     if len(msg) == 0:
         return None, None
