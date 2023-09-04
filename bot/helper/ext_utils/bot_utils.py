1707d37f84372f09432a42d19e8619a9145cb2b3
amitsha1 <121236130+amitsha1@users.noreply.github.com>
2023-09-04 10:20:40 +0530

    Update bot_utils.py

--- bot/helper/ext_utils/bot_utils.py
+++ bot/helper/ext_utils/bot_utils.py
@@ -149,8 +149,8 @@ def get_readable_message():
                                      MirrorStatus.STATUS_QUEUEUP, MirrorStatus.STATUS_LOCAL]:
 
            
-            msg += f"\n┎•❆﹝{get_progress_bar_string(download.progress())}﹞⌇{download.progress()}"
-            msg += f"\n┠•⍟ Done » {download.processed_bytes()} of {download.size()}"
+            msg += f"\n┎•❆﹝{get_progress_bar_string(download.progress())}"
+            msg += f"\n┠•⍟ Done » {download.processed_bytes()} of {download.size()} | {download.progress()}"
             msg += f"\n┠•🚀 Speed » {download.speed()}"
             msg += f"\n┠•⌥ ETA » {download.eta()}"
             msg += f"┠•☋ Elapsed » {get_readable_time(elapsed)}"
@@ -159,7 +159,7 @@ def get_readable_message():
             if hasattr(download, 'playList'):
                 try:
                     if playlist:=download.playList():
-                        msg += f"\n• <code>YT Count </code>» {playlist}"
+                        msg += f"\n┠• YT Count </code>» {playlist}"
                 except:
                     pass
 
@@ -182,10 +182,8 @@ def get_readable_message():
         if config_dict['DELETE_LINKS']:
             msg += f"\n┠• ☋ Task » {download.extra_details['mode']}"
         else:
-            msg += f"\n• <☋ Task » <a href='{download.message.link}'>{download.extra_details['mode']}</a>"
-
-        msg += f"\n┠•⌼ User » </code> {tag}"
-        msg += f"\n┖⚠️ /{BotCommands.CancelMirror}_{download.gid()}\n\n"
+            msg += f"\n• <☋ Task » <a href='{download.message.link}'>{download.extra_details['mode']}</a>" msg += f"\n┠•⌼ User » </code> {tag}"
+        msg += f"\n┖⌀ /{BotCommands.CancelMirror}_{download.gid()}\n\n"
 
     if len(msg) == 0:
         return None, None
@@ -215,7 +213,7 @@ def get_readable_message():
         buttons.ibutton(f"{PAGE_NO}/{PAGES}", "status ref")
         buttons.ibutton("⫸", "status nex")
         button = buttons.build_menu(3)
-    msg += "BOT STATS"
+    msg += "❆   ﹝BOT STATICS﹞"
     msg += f"\n<b>✇ DISK</b>: {get_readable_file_size(disk_usage(config_dict['DOWNLOAD_DIR']).free)}</code>"
     msg += f" | <b>🝋 Uptime</b>: {get_readable_time(time() - botStartTime)}</code>"
     msg += f"\n<b>⥥ Down</b>: {get_readable_file_size(dl_speed)}/s</code>"
