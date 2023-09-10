c876586db53d390414e0e9bff953ec27d94d2f83
amitsha1 <121236130+amitsha1@users.noreply.github.com>
2023-09-10 19:00:11 +0530

    Update bot_utils.py

--- bot/helper/ext_utils/bot_utils.py
+++ bot/helper/ext_utils/bot_utils.py
@@ -216,7 +216,6 @@ def get_readable_message():
         buttons.ibutton("⫸", "status nex")
         button = buttons.build_menu(3)
     msg += "____________________________"
-    msg += f"\n<b>✇ CPU</b>: {cpu_percent()}% | <b>⌭ RAM</b>: {virtual_memory().percent}%"
     msg += f"\n<b>✇ DISK</b>: {get_readable_file_size(disk_usage(config_dict['DOWNLOAD_DIR']).free)}</code>"
     msg += f" | <b>🝋 Uptime</b>: {get_readable_time(time() - botStartTime)}</code>"
     msg += f"\n<b>⥥ Down</b>: {get_readable_file_size(dl_speed)}/s</code>"
