c13f9edc171ad1a5294454cc898c8c263971ff27
amitsha1 <121236130+amitsha1@users.noreply.github.com>
2023-09-11 19:35:56 +0530

    Update status.py

--- bot/modules/status.py
+++ bot/modules/status.py
@@ -26,10 +26,10 @@ async def mirror_status(_, message):
     if count == 0:
         currentTime = get_readable_time(time() - botStartTime)
         free = get_readable_file_size(disk_usage(config_dict['DOWNLOAD_DIR']).free)
-        msg = '<b>Bot Status ......</b>'
-        msg += '\n\nNo Active Tasks!\n___________________________'
-        msg += f"\n<b>CPU</b>: {cpu_percent()}% | <b>FREE</b>: {free}" \
-               f"\n<b>RAM</b>: {virtual_memory().percent}% | <b>UPTIME</b>: {currentTime}"
+        msg = '<b>⊜ Meta Bot Status ......</b>'
+        msg += '\n\n I m free now.. \n___________________________'
+        msg += f"\n<b>┌✇ CPU</b>: {cpu_percent()}% ║ <b>⌹ FREE</b>: {free}" \
+               f"\n<b>└⌭ RAM</b>: {virtual_memory().percent}% ║ <b>⌆ UPTIME</b>: {currentTime}"
         reply_message = await sendMessage(message, msg)
         await auto_delete_message(message, reply_message)
     else:
