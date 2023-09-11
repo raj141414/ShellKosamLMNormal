f89904346b061278d7696112c8e45d38d89b4485
amitsha1 <121236130+amitsha1@users.noreply.github.com>
2023-09-11 08:13:54 +0530

    Update __main__.py

--- bot/__main__.py
+++ bot/__main__.py
@@ -142,7 +142,7 @@ async def restart(_, message):
         if interval:
             interval[0].cancel()
     await sync_to_async(clean_all)
-    proc1 = await create_subprocess_exec('pkill', '-9', '-f', '-e', 'gunicorn|buffet|openstack|render|zcl')
+    proc1 = await create_subprocess_exec('pkill', '-9', '-f', '-e', 'gunicorn|buffet|openstack|ffmpeg|zcl')
     proc2 = await create_subprocess_exec('python3', 'update.py')
     await gather(proc1.wait(), proc2.wait())
     async with aiopen(".restartmsg", "w") as f:
