0855c13f1e887233098486f96bdfebaa8b530b2d
Abdullah Al Noman <101085500+AtrociousMirrorBot@users.noreply.github.com>
2023-09-19 14:19:41 +0600

    Update bot_utils.py

--- bot/helper/ext_utils/bot_utils.py
+++ bot/helper/ext_utils/bot_utils.py
@@ -228,8 +228,8 @@ def get_readable_message():
     msg += "____________________________"
     msg += f"\n<b>✇ DISK</b>: {get_readable_file_size(disk_usage(config_dict['DOWNLOAD_DIR']).free)}</code>"
     msg += f" ║ <b>🝋 Uptime</b>: {get_readable_time(time() - botStartTime)}</code>"
-    msg += f"\n<b>⥥ Down</b>: {get_readable_file_size(dl_speed)}/s</code>"
-    msg += f" ║ <b>⥣ Up</b>: {get_readable_file_size(up_speed)}/s</code>"
+    msg += f"\n<b>⥥ Down</b>: {get_readable_file_size(dl_speed)}/s</code> ║ <b>⥣ Up</b>: {get_readable_file_size(up_speed)}/s</code>"
+    msg += f"\n<b>CPU:</b> {cpu_percent()}% | <b>RAM:</b> {virtual_memory().percent}%"
     remaining_time = 86400 - (time() - botStartTime)
     res_time = '⚠️ ANYTIME ⚠️' if remaining_time <= 0 else get_readable_time(remaining_time)
     if remaining_time <= 3600:
