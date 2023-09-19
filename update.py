acc0743c0b081756c92843d95a0f259002c11bb8
Abdullah Al Noman <101085500+AtrociousMirrorBot@users.noreply.github.com>
2023-09-19 12:43:54 +0600

    Update update.py

--- update.py
+++ update.py
@@ -1,39 +1,26 @@
 from logging import FileHandler, StreamHandler, INFO, basicConfig, error as log_error, info as log_info
-from os import path as ospath, environ, execl as osexecl
+from os import path as ospath, environ, remove
 from subprocess import run as srun
 from requests import get as rget
-from dotenv import load_dotenv
-from sys import executable
+from dotenv import load_dotenv, dotenv_values
 from pymongo import MongoClient
 
-if ospath.exists('Z_Logs.txt'):
-    with open('Z_Logs.txt', 'r+') as f:
+if ospath.exists('log.txt'):
+    with open('log.txt', 'r+') as f:
         f.truncate(0)
 
-basicConfig(format='%(levelname)s | From %(name)s -> %(module)s line no: %(lineno)d | %(message)s',
-                    handlers=[FileHandler('Z_Logs.txt'), StreamHandler()], level=INFO)
+if ospath.exists('rlog.txt'):
+    remove('rlog.txt')
 
-CONFIG_FILE_URL = environ.get('CONFIG_FILE_URL')
-try:
-    if len(CONFIG_FILE_URL) == 0:
-        raise TypeError
-    try:
-        res = rget(CONFIG_FILE_URL)
-        if res.status_code == 200:
-            with open('config.env', 'wb+') as f:
-                f.write(res.content)
-        else:
-            log_error(f"Failed to download config.env {res.status_code}")
-    except Exception as e:
-        log_error(f"CONFIG_FILE_URL: {e}")
-except:
-    pass
+basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+            handlers=[FileHandler('log.txt'), StreamHandler()],
+            level=INFO)
 
 load_dotenv('config.env', override=True)
 
 try:
     if bool(environ.get('_____REMOVE_THIS_LINE_____')):
-        log_error('The README.md file there to read! Exiting now!')
+        log_error('The README.md file there to be read! Exiting now!')
         exit()
 except:
     pass
@@ -49,41 +36,42 @@ DATABASE_URL = environ.get('DATABASE_URL', '')
 if len(DATABASE_URL) == 0:
     DATABASE_URL = None
 
-if DATABASE_URL:
+if DATABASE_URL is not None:
     conn = MongoClient(DATABASE_URL)
-    db = conn.z
-    # retrun config dict (all env vars)
-    if config_dict := db.settings.config.find_one({'_id': bot_id}):
+    db = conn.mltb
+    old_config = db.settings.deployConfig.find_one({'_id': bot_id})
+    config_dict = db.settings.config.find_one({'_id': bot_id})
+    if old_config is not None:
+        del old_config['_id']
+    if (old_config is not None and old_config == dict(dotenv_values('config.env')) or old_config is None) \
+            and config_dict is not None:
         environ['UPSTREAM_REPO'] = config_dict['UPSTREAM_REPO']
         environ['UPSTREAM_BRANCH'] = config_dict['UPSTREAM_BRANCH']
     conn.close()
 
 UPSTREAM_REPO = environ.get('UPSTREAM_REPO', '')
 if len(UPSTREAM_REPO) == 0:
-    UPSTREAM_REPO = 'https://gitlab.com/Dawn-India/Z-Mirror'
+    UPSTREAM_REPO = None
 
 UPSTREAM_BRANCH = environ.get('UPSTREAM_BRANCH', '')
 if len(UPSTREAM_BRANCH) == 0:
-    UPSTREAM_BRANCH = 'zh_run'
+    UPSTREAM_BRANCH = 'master'
 
-if ospath.exists('.git'):
-    srun(["rm", "-rf", ".git"])
+if UPSTREAM_REPO is not None:
+    if ospath.exists('.git'):
+        srun(["rm", "-rf", ".git"])
 
-update = srun([f"git init -q \
-                 && git config --global user.email dawn-in@z-mirror.live \
-                 && git config --global user.name z-mirror \
-                 && git add . \
-                 && git commit -sm update -q \
-                 && git remote add origin {UPSTREAM_REPO} \
-                 && git fetch origin -q \
-                 && git reset --hard origin/{UPSTREAM_BRANCH} -q"], shell=True)
+    update = srun([f"git init -q \
+                     && git config --global user.email e.anastayyar@gmail.com \
+                     && git config --global user.name mltb \
+                     && git add . \
+                     && git commit -sm update -q \
+                     && git remote add origin {UPSTREAM_REPO} \
+                     && git fetch origin -q \
+                     && git reset --hard origin/{UPSTREAM_BRANCH} -q"], shell=True)
 
-if update.returncode == 0:
-    log_info('Successfully updated with latest commit.')
-    log_info(f'Repo in use: {UPSTREAM_REPO}')
-    log_info(f'Branch in use: {UPSTREAM_BRANCH}')
-    log_info('Thanks For Using Z_Mirror')
-else:
-    log_error('Something went wrong while updating.')
-    log_info('Check if entered UPSTREAM_REPO is valid or not!')
-    log_info(f'Entered upstream repo: {UPSTREAM_REPO}')
+    if update.returncode == 0:
+        log_info('Successfully updated with latest commit from UPSTREAM_REPO')
+    else:
+        log_error(
+            'Something went wrong while updating, check UPSTREAM_REPO if valid or not!')
