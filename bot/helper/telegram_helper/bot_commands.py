1a1be992c7e71ec1cb96ea841e6e35e1b2c4bcf7
amitsha1 <121236130+amitsha1@users.noreply.github.com>
2023-09-29 18:45:21 +0530

    Update bot_commands.py

--- bot/helper/telegram_helper/bot_commands.py
+++ bot/helper/telegram_helper/bot_commands.py
@@ -40,6 +40,6 @@ class _BotCommands:
         self.RmdbCommand = f'rmdb{CMD_SUFFIX}'
         self.MLocalUploadCommand = f'mupload{CMD_SUFFIX}'
         self.LLocalUploadCommand = f'lupload{CMD_SUFFIX}'
-        self.vtoolsCommand = f'vtl{CMD_SUFFIX}'
+        self.vid_toolsCommand = f'vtl{CMD_SUFFIX}'
 
 BotCommands = _BotCommands()
