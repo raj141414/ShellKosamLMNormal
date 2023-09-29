09e985c5cc9c677faa037549365bcd385729566f
amitsha1 <121236130+amitsha1@users.noreply.github.com>
2023-09-29 18:17:58 +0530

    Update vtools.py

--- bot/modules/vtools.py
+++ bot/modules/vtools.py
@@ -1,8 +1,8 @@
+from pyrogram.filters import command
+from pyrogram.handlers import MessageHandler
 from aiofiles.os import path as aiopath, makedirs
 from aioshutil import move as aiomove
 from os import path as ospath
-from pyrogram.filters import command
-from pyrogram.handlers import MessageHandler
 from pyrogram.types import Message
 from random import SystemRandom
 from string import ascii_letters, digits
@@ -16,7 +16,32 @@ from bot.helper.telegram_helper.bot_commands import BotCommands
 from bot.helper.telegram_helper.filters import CustomFilters
 from bot.helper.telegram_helper.message_utils import sendMessage, sendStatusMessage
 
+def send_menu(message):
+    # Create a custom keyboard (menu)
+    markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
+    item1 = types.KeyboardButton("Option 1")
+    item2 = types.KeyboardButton("Option 2")
+    item3 = types.KeyboardButton("Option 3")
+    markup.add(item1, item2, item3)
 
+    # Send a welcome message with the menu
+    bot.send_message(message.chat.id, "Welcome to the Bot Menu!", reply_markup=markup)
+
+# Define handlers for each menu option
+@bot.message_handler(func=lambda message: message.text == "Option 1")
+def option1(message):
+    bot.send_message(message.chat.id, "You selected Option 1!")
+
+@bot.message_handler(func=lambda message: message.text == "Option 2")
+def option2(message):
+    bot.send_message(message.chat.id, "You selected Option 2!")
+
+@bot.message_handler(func=lambda message: message.text == "Option 3")
+def option3(message):
+    bot.send_message(message.chat.id, "You selected Option 3!")
+
+# Polling to continuously check for new messages
+bot.polling()
 
 
 
